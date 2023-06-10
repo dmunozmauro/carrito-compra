9f5cf46b2e718dac0bacec0bd85ed72f06d581d5
dmunoz <dmunoz@aligare.cl>
2023-06-10 02:57:46 -0400

    avance carrito

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import routes from "./routes";
-import { createBrowserHistory } from "history";
 import './App.css';
-import { HomeComponent } from "./components/HomeComponent";
 
-const historial = createBrowserHistory();
 
 function App() {
-
-  /* const RouteWithSubRoutes = (routes) => (
-    <Route
-      path={routes.path}
-      exact={routes.exact}
-      key={`route${routes.path}`}
-      render={(props) => {
-        return (<route.Component {...props} routes={routes.routes} />)
-      }}
-    />
-  ); */
-
-
-
   return (
-
-
-    <Router>
-      <Routes>
-        {
-          routes.map((r) => {
-            console.log(r.Component.name);
-          })
-        }
-      </Routes>
-    </Router>
+    <div className="container-fluid m-5">
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route, i) => (
+            <Route key={i} exact path={route.path} Component={route.Component} />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/HomeComponent/index.jsx
+++ src/components/HomeComponent/index.jsx
@@ -1,11 +1,94 @@
 import React from "react";
+import { useEffect, useState } from 'react';
 
 export const HomeComponent = () => {
 
-return (
-    <div>
-        <h1>Hola react!</h1>
-    </div>
-) 
+    const [dataInventario, setDataInventario] = useState([])
+
+    useEffect(() => {
+        getProductos();
+        seeCarrito();
+    }, []);
+    
+
+    const seeCarrito = async () => {
+        try {
+
+            const url = `http://localhost:5000/carrito/ver-productos-carrito`;
+            const opcionesRequest = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            };
+            const responseFetch = await fetch(url, opcionesRequest);
+            const responseJson = await responseFetch.json();
+
+        } catch (err) {
+            console.log("err: ", err);
+        }
+    }
+
+    const getProductos = async () => {
+        try {
+
+            const url = `http://localhost:5000/productos/obtener-productos`;
+            const opcionesRequest = {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            };
+            const responseFetch = await fetch(url, opcionesRequest);
+            const responseJson = await responseFetch.json();
+            setDataInventario(responseJson.data);
+
+        } catch (err) {
+            console.log("err: ", err);
+        }
+    }
+
+    const addCarrito = async (id) => {
+        try {
+            const url = `http://localhost:5000/carrito/agregar-producto-carrito`;
+            const body = { id_producto: id };
+
+            const opcionesPeticion = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            };
+
+            const responseFetch = await fetch(url, opcionesPeticion);
+            const responseJson = await responseFetch.json();
+            console.log('responseJson', responseJson);
+
+        } catch (err) {
+            console.log("err: ", err);
+        }
+
+
+    }
+
+    return (
+        <div className="row">
+            {dataInventario.map((d, i) => {
+                return (
+                    <div key={i} className="col-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{d.nombre}</h5>
+                                <p>{d.valor}</p>
+                                <button type="button" onClick={() => addCarrito(d.id)} className="btn btn-primary">Agregar</button>
+                            </div>
+                        </div>
+
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
 
